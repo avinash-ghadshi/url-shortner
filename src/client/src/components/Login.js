00b6551faeb004c34791c11740c34f586ba84403
avinash ghadshi <avinash.ghadshi@netcorecloud.com>
2024-06-27 10:10:32 +0530

    First commit

---
+++ src/client/src/components/Login.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './../css/login.css'
+
+const Login = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Username:', username);
+        console.log('Password:', password);
+    }
+
+    return (
+        <div className="login-container">
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="username">Username:</label>
+                  <input
+                    type="text"
+                    id="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password:</label>
+                  <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+      );
+}
+
+export default Login
