4741e56cb982cd1f4761bbf88ac8eb586955681d
avinash ghadshi <avinash.ghadshi@netcorecloud.com>
2024-06-27 10:59:33 +0530

    Login page changes

--- src/client/src/components/Login.js
+++ src/client/src/components/Login.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import './../css/login.css'
+import './../css/login.css';
 
 const Login = () => {
-
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
@@ -10,36 +9,62 @@ const Login = () => {
         event.preventDefault();
         console.log('Username:', username);
         console.log('Password:', password);
-    }
+    };
 
     return (
-        <div className="login-container">
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                  <label htmlFor="username">Username:</label>
-                  <input
-                    type="text"
-                    id="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                  />
+        <div className="container">
+            <div className="form-box">
+                <div className="header-form">
+                    <h4 className="text-primary text-center">
+                        <i className="fa fa-user-circle" style={{ fontSize: "110px" }}></i>
+                    </h4>
+                    <div className="image"></div>
                 </div>
-                <div className="form-group">
-                  <label htmlFor="password">Password:</label>
-                  <input
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                  />
+                <div className="body-form">
+                    <form>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text"><i className="fa fa-user"></i></span>
+                            </div>
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Username"
+                                id="username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text"><i className="fa fa-lock"></i></span>
+                            </div>
+                            <input
+                                type="password"  // Corrected to type="password"
+                                className="form-control"
+                                placeholder="Password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <button type="button" onClick={handleSubmit} className="btn btn-secondary btn-block">LOGIN</button>
+                        <div className="message">
+                            <div><input type="checkbox" /> Remember ME</div>
+                            <div><a href="#">Forgot your password</a></div>
+                        </div>
+                    </form>
+                    <div className="social">
+                        <a href="#"><i className="fab fa-facebook"></i></a>
+                        <a href="#"><i className="fab fa-twitter-square"></i></a>
+                        <a href="#"><i className="fab fa-google"></i></a>
+                    </div>
                 </div>
-                <button type="submit">Login</button>
-            </form>
+            </div>
         </div>
-      );
-}
+    );
+};
 
-export default Login
+export default Login;
